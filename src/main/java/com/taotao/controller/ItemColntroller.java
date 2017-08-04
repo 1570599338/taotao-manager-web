aec0969eb37bfdd9837fd1fe82fc8b000c5547a3
lquan <1570599338@qq.com>
2017-08-04 22:54:49 +0800

    web提交

---
+++ src/main/java/com/taotao/controller/ItemColntroller.java
@@ -0,0 +1,32 @@
+package com.taotao.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+
+/**
+ * 商品管理Controller
+ * @author lquan
+ *
+ */
+@Controller
+public class ItemColntroller {
+	
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable Long itemId){
+		TbItem tbIem = itemService.getItemById(itemId);
+		
+		return tbIem;
+	}
+	
+
+}
