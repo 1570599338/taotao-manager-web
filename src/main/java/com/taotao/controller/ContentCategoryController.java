3f11f72533957b1cbd7a826a5369c8214972d7fa
lquan <1570599338@qq.com>
2017-08-11 01:46:57 +0800

    web的controller的商品分类展示

---
+++ src/main/java/com/taotao/controller/ContentCategoryController.java
@@ -0,0 +1,40 @@
+package com.taotao.controller;
+
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.common.pojo.EasyUITreeNode;
+import com.taotao.content.service.ContentCategoryService;
+
+/**
+ * 内容分类管理Controller
+ * @author lquan
+ *
+ */
+@Controller
+public class ContentCategoryController {
+	
+	@Resource(name="contentCategoryServiceA")
+	private ContentCategoryService contentCategoryService;
+	
+	
+	/**
+	 * 获取分类信息
+	 * @param parentId
+	 * @return
+	 */
+	@RequestMapping("/content/category/list")
+	@ResponseBody
+	public List<EasyUITreeNode> getContentCategory(@RequestParam(value="id",defaultValue="0")Long parentId){
+		List<EasyUITreeNode> list = this.contentCategoryService.getContentCategoryList(parentId);
+		return list;
+	}
+	
+}
